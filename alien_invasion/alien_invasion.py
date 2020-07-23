514b0502fb2378af3f18ba8fbe2cfe7f6da0cd1f
jamalfox85 <66087193+jfox55@users.noreply.github.com>
2020-07-23 13:20:36 -0400

    Ship
    Added ship image to window

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -14,7 +15,6 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.ship = Ship(self)
         self.ship = Ship(self)
 
     def run_game(self):
